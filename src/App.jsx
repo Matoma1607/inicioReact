da9bc29011f66b1fc30b705578defef935cd547c
Matias Salazar <matias39974593@gmail.com>
2024-08-14 11:33:43 -0300

    se agrego un poco de contenido al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion";
+
 function App() {
-  return <div></div>;
+  // aqui agregar logica de un componente
+
+  return (
+    <>
+      <section className="container my-5">
+      <h1 className="text-center">Practica inicial de React</h1>
+      {/* aqui voy agregar un componente */}
+      <Informacion/>
+    </section>
+  
+    </>
+    
+  );
 }
 
 export default App;
